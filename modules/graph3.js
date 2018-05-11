62f4558d1aa338cbbdd2de084931e2f4169aa0a2
yoann ochietti <yoann.ochietti@gmail.com>
2018-05-11 19:34:03 -0400

    Linting

--- modules/graph3.js
+++ modules/graph3.js
@@ -3,69 +3,69 @@
 	'use strict';
 	/* global require */
 
-	var VegaWrapper = require( 'yochie-mw-graph-shared' );
+	var VegaWrapper = require( 'yochie-mw-graph-shared' ),
 
-	// eslint-disable-next-line no-new
-	var wrappedVega = new VegaWrapper({
-		data: {
-			extend: vg.extend,
-			loader: vg.loader,
-			http: vg.loader().http
-		},
-		isTrusted: mw.config.get( 'wgGraphIsTrusted' ),
-		domains: mw.config.get( 'wgGraphAllowedDomains' ),
-		domainMap: false,
-		logger: function ( warning ) {
-			mw.log.warn( warning );
-		},
-		parseUrl: function ( url, opt ) {
-			// Parse URL
-			var uri = new mw.Uri( url );
+		// eslint-disable-next-line no-new
+		wrappedVega = new VegaWrapper( {
+			data: {
+				extend: vg.extend,
+				loader: vg.loader,
+				http: vg.loader().http
+			},
+			isTrusted: mw.config.get( 'wgGraphIsTrusted' ),
+			domains: mw.config.get( 'wgGraphAllowedDomains' ),
+			domainMap: false,
+			logger: function ( warning ) {
+				mw.log.warn( warning );
+			},
+			parseUrl: function ( url ) {
+				// Parse URL
+				var uri = new mw.Uri( url );
 
-			// reduce confusion, only keep expected values
-			if ( uri.port ) {
-				uri.host += ':' + uri.port;
-				delete uri.port;
-			}
-			// If url begins with   protocol:///...  mark it as having relative host
-			if ( /^[a-z]+:\/\/\//.test( url ) ) {
-				uri.isRelativeHost = true;
-			}
-			if ( uri.protocol ) {
-				// All other libs use trailing colon in the protocol field
-				uri.protocol += ':';
-			}
-			// Node's path includes the query, whereas pathname is without the query
-			// Standardizing on pathname
-			uri.pathname = uri.path;
-			delete uri.path;
-			return uri;
-		},
-		formatUrl: function ( uri, opt ) {
-			// Format URL back into a string
-			// Revert path into pathname
-			uri.path = uri.pathname;
-			delete uri.pathname;
-
-			if ( location.host.toLowerCase() === uri.host.toLowerCase() ) {
-				if ( !mw.config.get( 'wgGraphIsTrusted' ) ) {
-					// Only send this header when hostname is the same.
-					// This is broader than the same-origin policy,
-					// but playing on the safer side.
-					opt.headers = { 'Treat-as-Untrusted': 1 };
+				// reduce confusion, only keep expected values
+				if ( uri.port ) {
+					uri.host += ':' + uri.port;
+					delete uri.port;
 				}
-			} else if ( opt.addCorsOrigin ) {
-				// All CORS api calls require origin parameter.
-				// It would be better to use location.origin,
-				// but apparently it's not universal yet.
-				uri.query.origin = location.protocol + '//' + location.host;
-			}
+				// If url begins with   protocol:///...  mark it as having relative host
+				if ( /^[a-z]+:\/\/\//.test( url ) ) {
+					uri.isRelativeHost = true;
+				}
+				if ( uri.protocol ) {
+					// All other libs use trailing colon in the protocol field
+					uri.protocol += ':';
+				}
+				// Node's path includes the query, whereas pathname is without the query
+				// Standardizing on pathname
+				uri.pathname = uri.path;
+				delete uri.path;
+				return uri;
+			},
+			formatUrl: function ( uri, opt ) {
+				// Format URL back into a string
+				// Revert path into pathname
+				uri.path = uri.pathname;
+				delete uri.pathname;
 
-			uri.protocol = VegaWrapper.removeColon( uri.protocol );
-			return uri.toString();
-		},
-		languageCode: mw.config.get( 'wgUserLanguage' )
-	} );
+				if ( location.host.toLowerCase() === uri.host.toLowerCase() ) {
+					if ( !mw.config.get( 'wgGraphIsTrusted' ) ) {
+						// Only send this header when hostname is the same.
+						// This is broader than the same-origin policy,
+						// but playing on the safer side.
+						opt.headers = { 'Treat-as-Untrusted': 1 };
+					}
+				} else if ( opt.addCorsOrigin ) {
+					// All CORS api calls require origin parameter.
+					// It would be better to use location.origin,
+					// but apparently it's not universal yet.
+					uri.query.origin = location.protocol + '//' + location.host;
+				}
+
+				uri.protocol = VegaWrapper.removeColon( uri.protocol );
+				return uri.toString();
+			},
+			languageCode: mw.config.get( 'wgUserLanguage' )
+		} );
 
 	/**
 	 * Set up drawing canvas inside the given element and draw graph data
@@ -73,15 +73,15 @@
 	 * @param {Object|string} data graph spec
 	 */
 	mw.drawVegaGraph = function ( element, data ) {
-		let view;
-		function render(spec) {
-			view = new vg.View ( vg.parse(spec), {loader: wrappedVega.data.loader()} )
+
+		function render( spec ) {
+			new vg.View( vg.parse( spec ), { loader: wrappedVega.data.loader() } )
 				.renderer( 'svg' )
 				.initialize( element )
 				.hover()
 				.run();
 		}
-		render(data);
+		render( data );
 	};
 
 	mw.hook( 'wikipage.content' ).add( function ( $content ) {
